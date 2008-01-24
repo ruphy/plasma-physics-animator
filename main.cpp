8747dd6d0c37c76f3cf9bc2a4c2efae1fb86232a
Riccardo Iaconelli <riccardo@kde.org>
2008-01-24 02:33:21 +0100

    Commit that breaks something, seems that we never return 0, but makes the whole code much more ordinated.

--- main.cpp
+++ main.cpp
@@ -2,44 +2,57 @@
 
 using namespace std;
 
-int getXRebounce(float t, float v0 = 0);
+float s0=0;
 
-int main()
+float getXInRebounce(float time, float initialSpeed = 0)
 {
     int m = 1;
-    double time = 0.01;
+//     double time = 0.01;
     double mu = 0.5;
     float g  = 9.81;
     float k = 9.81;
     float deltax = 1;
     float speed = 0;
 
-    for (time=0; time<1; time+=0.01) { 
+    if (initialSpeed != s0) {
+      s0 = initialSpeed;
+    }
+
+    float vincularForce = m*g;
+    float friction = vincularForce*mu;
+    float springForce = deltax*k;
+    float totalForce = springForce - friction;
+
+    if ((speed < 0.1 && totalForce < 0.1) || deltax < 0.1) {
+	totalForce = 0;
+	speed = 0;
+	deltax = 0;
+	return 0;
+    }
+
+    float a = totalForce/m;
+    //float a=1;
+
+    speed += a*time;
+
+    deltax -= speed*time;
+
+    cout << "time is:      " << time <<  " and the spring force is:   " << springForce  << endl;
+    cout << "a is:   " << a << " while delta X is:    " << deltax << endl << endl;
+
+    return deltax;
+}
+
+int main()
+{
 
 
-        float vincularForce = m*g;
-        float friction = vincularForce*mu;
-        float springForce = deltax*k;
-	float totalForce = springForce - friction;
+    for (double t=0; t<1; t+=0.01) { 
 
-	if ((speed < 0.1 && totalForce < 0.1) || deltax < 0.1) {
-		totalForce = 0;
-		speed = 0;
-                deltax = 0;
-		break;
+	if (getXInRebounce(t) == 0) {
+	    break;
 	}
-
-	float a = totalForce/m;
-        //float a=1;
-
-        speed += a*time;
-
-	deltax -= speed*time;
-
-        cout << "time is:      " << time <<  " and the spring force is:   " << springForce  << endl;
-        cout << "a is:   " << a << " while delta X is:    " << deltax << endl << endl;
-
-     }
+    }
 
 }
 
